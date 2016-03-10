3c9bf1afbcf52c45b91f73d90ef99418af4046b2
Manfred Touron <m@42.am>
2016-03-10 17:24:48 +0100

    Add new-image-release.sh

---
+++ new-image-release.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+
+# user input
+image=$1
+
+
+# variables
+set -x
+machine_name=$(echo $image | tr "/" "_")
+hostname=$(echo $image | cut -d/ -f1)
+user=$(echo $image | cut -d/ -f2)
+repo=$(echo $image | cut -d/ -f3)
+subdir=$(echo $image | cut -d/ -f4-)
+repo_clean=$(echo $repo | sed 's/^scaleway-//;s/^image-//')
+tag=$(echo $subdir | tr / -)
+docker_name=$user/$repo_clean:$tag
+
+
+# create build request on the repo
+pushd images/$image
+git clean -fxd .
+latest=$(ls | sort -n | tail -n 1)
+next_id=$(echo "$latest+1" | bc)
+branch_name=build-image-$user-$repo_clean-$tag-$next_id
+mkdir -p $next_id
+touch $next_id/.build
+git checkout -b $branch_name
+git add $next_id/.build
+git commit $next_id/.build -m "Trigger build of $docker_name rev$next_id :gun:"
+git push -u origin $branch_name
+git checkout master
